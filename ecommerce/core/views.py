9134a0522715d8173612f1849fd123432b476019
talha485 <talhachaudhary120@gmail.com>
2026-02-17 16:57:01 +0500

    Week 2: dynamic home page + search + filtering

--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -8,9 +8,32 @@ def home(request):
     return render(request, 'home.html')
 
 def product_list(request):
+    query = request.GET.get("q")
+    category = request.GET.get("category")
+
     products = Product.objects.all()
-    return render(request, 'products.html', {'products': products})
+
+    if query:
+        products = products.filter(name__icontains=query)
+
+    if category:
+        products = products.filter(category__iexact=category)
+
+    return render(request, "products.html", {
+        "products": products
+    })
+
+
 
 def product_detail(request, id):
     product = get_object_or_404(Product, id=id)
     return render(request, 'product_detail.html', {'product': product})
+
+from .models import Product
+
+def home(request):
+    featured_products = Product.objects.all()[:4]  # first 4
+    return render(request, "home.html", {
+        "products": featured_products
+    })
+
